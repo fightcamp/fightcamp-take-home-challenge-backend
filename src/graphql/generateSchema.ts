983484d0c032fb82bb4b9802d4391c02fba2ad18
Louis-Mathieu Houle <louis-mathieu@planned.com>
2023-05-02 16:06:10 -0400

    remove deprecated "graphql-tools", now using @graphql-tools/schema

--- src/graphql/generateSchema.ts
+++ src/graphql/generateSchema.ts
@@ -1,6 +1,6 @@
 import 'graphql-import-node';
 import * as rootDefs from './schema/schema.graphql';
-import { makeExecutableSchema } from 'graphql-tools';
+import { makeExecutableSchema } from '@graphql-tools/schema'
 import resolvers from './resolvers/resolvers';
 
 export const schema = makeExecutableSchema({
